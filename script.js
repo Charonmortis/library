fb7b5087ecdd3cded786455de4bbc286b89802cb
Charonmortis <101378229+Charonmortis@users.noreply.github.com>
2023-01-21 20:40:32 +0100

    DOM elements declared and addBooks function cleaned

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 let libraryBooks = []
 
-function book(name, year, author, read){
-    this.name = name
-    this.year = year
+function book(title, author, read, pages){
+    this.title = title
     this.author = author
+    this.pages = pages
     this.read = read
 }
 
-const addBooks = (name, year, author, read)=>{
-    libraryBooks.push(new book(name, year, author, read))
-}
-
-addBooks('Gloria', '1000', 'Graynor', false)
-
-console.log(libraryBooks)
 
 const showBooks = ()=>{
     libraryBooks.forEach(element => {
-        console.log(element)
+        // SHOW THE BOOKS
     });
 }
 
-showBooks()
+//Making UI functional
+
+const FORM_TITLE = document.querySelector('#title').value
+const FORM_AUTHOR = document.querySelector('#author').value
+const FORM_PAGES = document.querySelector('#pages').value
+const FORM_READ_STATUS = document.querySelector('#read').value
+const SUBMIT_BUTTON = document.querySelector('#read')
+
+const addBooks = ()=>{
+    let title = FORM_TITLE
+    let author = FORM_AUTHOR
+    let read = FORM_READ_STATUS
+    let pages = FORM_PAGES
+
+    libraryBooks.push(new book(title, author, read, pages))
+    console.log(libraryBooks)
+}
+
+SUBMIT_BUTTON.addEventListener('click', addBooks())
